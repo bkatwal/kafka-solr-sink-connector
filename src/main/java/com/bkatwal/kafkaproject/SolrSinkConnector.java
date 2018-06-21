2671987f76ba103949aae1d33e6cc6251e985520
Bikas <bikas.katwal10@gmail.com>
2018-06-22 00:53:26 +0530

    Added licence

--- src/main/java/com/bkatwal/kafkaproject/SolrSinkConnector.java
+++ src/main/java/com/bkatwal/kafkaproject/SolrSinkConnector.java
@@ -1,14 +1,28 @@
-package com.bkatwal.kafkaproject;
+/**
+ * Copyright 2018 Bikas Katwal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **/
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+package com.bkatwal.kafkaproject;
 
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.sink.SinkConnector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class SolrSinkConnector extends SinkConnector {
 
@@ -16,7 +30,7 @@ public class SolrSinkConnector extends SinkConnector {
 
   @Override
   public String version() {
-    return VersionUtil.getVersion();
+    return VersionUtil.getVersion(SolrSinkConnector.class);
   }
 
   @Override
--- src/main/java/com/bkatwal/kafkaproject/VersionUtil.java
+++ src/main/java/com/bkatwal/kafkaproject/VersionUtil.java
@@ -1,15 +1,40 @@
+/**
+ * Copyright 2018 Bikas Katwal.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **/
+
 package com.bkatwal.kafkaproject;
 
-/**
- * Created by jeremy on 5/3/16.
- */
-class VersionUtil {
+import org.apache.solr.common.StringUtils;
 
-  public static String getVersion() {
+
+public class VersionUtil {
+  final static String DEFAULT = "0.0.0.0";
+
+  public static String getVersion(Class<?> cls) {
+    String result;
+
     try {
-      return VersionUtil.class.getPackage().getImplementationVersion();
-    } catch(Exception ex){
-      return "0.0.0.0";
+      result = cls.getPackage().getImplementationVersion();
+
+      if (StringUtils.isEmpty(result)) {
+        result = DEFAULT;
+      }
+    } catch (Exception ex) {
+      result = DEFAULT;
     }
+    return result;
   }
+
 }
